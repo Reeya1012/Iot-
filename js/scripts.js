f5883ce4d5a63e44f13daca2f4d36de9dfca5a0d
Bhavya-02022000 <bhavya.joshi@sakec.ac.in>
2021-03-24 18:56:16 +0530

    testimonial, about us

--- js/scripts.js
+++ js/scripts.js
@@ -54,3 +54,59 @@
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
+
+// testimonial slider 
+(()=>{
+    const sliderContainer = document.querySelector(".testi-slider-container"),
+    slides = sliderContainer.querySelectorAll(".testi-item"),
+    slideWidth = sliderContainer.offsetWidth,
+    prevBtn = document.querySelector(".testi-slider-nav .prev"),
+    nextBtn = document.querySelector(".testi-slider-nav .next"),
+    activeSlide = sliderContainer.querySelector(".testi-item.active");
+    let slideIndex = Array.from(activeSlide.parentElement.children).indexOf(activeSlide);
+
+    // set width of all slides
+    slides.forEach((slide)=>{
+        slide.style.width = slideWidth + "px";
+    })
+    // set width of sliderContainer
+    sliderContainer.style.width = slideWidth * slides.length + "px";
+    nextBtn.addEventListener("click", ()=>{
+        if(slideIndex === slides.length-1){
+            slideIndex = 0;
+        }
+        else{
+            slideIndex++;
+        }
+        slider();
+
+    })
+
+    prevBtn.addEventListener("click",()=>{
+        if(slideIndex === 0){
+            slideIndex = slides.length-1;
+        }
+        else{
+            slideIndex--;
+        }
+        slider();
+    })
+
+    function slider(){
+        // deactivate existing active slides
+        sliderContainer.querySelector(".testi-item.active").classList.remove("active");
+        // activate new slide
+        slides[slideIndex].classList.add("active");
+        sliderContainer.style.marginLeft = - (slideWidth * slideIndex) + "px";
+    }
+    slider();
+
+})();
+
+window.addEventListener("load", ()=>{
+    // preloader
+    document.querySelector(".preloader").classList.add("fade-out");
+    setTimeout(()=>{
+        document.querySelector(".preloader").style.display="none";
+    },600)
+})
